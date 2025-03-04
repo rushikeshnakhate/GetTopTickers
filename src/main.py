fc73b210c23c029034075ffb3bd285d1fac8cf32
Rushikesh <rushikeshnakhate@gmail.com>
2025-03-04 17:11:33 +0800

    date range changes

--- src/main.py
+++ src/main.py
@@ -28,54 +28,59 @@ def run_pyport_ticker_selector(
 ):
     # Generate date ranges (assuming generate_month_date_ranges is defined)
 
-    date_ranges = DateRangeGenerator(years=years, months=months, rebalancing_days=rebalancing_days,
+    date_ranges = DateRangeGenerator(years=years,
+                                     months=months,
+                                     rebalancing_days=rebalancing_days,
                                      rebalancing_months=rebalancing_months).get_date_range()
 
     results = []
     # Loop through date ranges and process the data
     for start_date, end_date in date_ranges:
-        logging.info("start_date={}, end_date={}".format(start_date, end_date))
+        try:
+            logging.info("start_date={}, end_date={}".format(start_date, end_date))
 
-        # Fetch market data
-        market_data = dataFetcher.get_close_price_service(ticker=GLobalColumnName.ticker_nifty50,
-                                                          start_date=start_date,
-                                                          end_date=end_date)
+            # Fetch market data
+            market_data = dataFetcher.get_close_price_service(ticker=GLobalColumnName.ticker_nifty50,
+                                                              start_date=start_date,
+                                                              end_date=end_date)
 
-        # Get the list of tickers
-        ticker_list, _ = get_stocks(tickers)
+            # Get the list of tickers
+            ticker_list, _ = get_stocks(tickers)
 
-        # Fetch close prices for all tickers
-        close_price = dataFetcher.get_close_price_service_bulk(ticker_list=ticker_list,
-                                                               start_date=start_date,
-                                                               end_date=end_date)
+            # Fetch close prices for all tickers
+            close_price = dataFetcher.get_close_price_service_bulk(ticker_list=ticker_list,
+                                                                   start_date=start_date,
+                                                                   end_date=end_date)
 
-        # Calculate indicators
-        indicators_df = get_indicator_bulk(ticker_data_df=close_price,
-                                           ticker_list=ticker_list,
-                                           start_date=start_date,
-                                           end_date=end_date)
+            # Calculate indicators
+            indicators_df = get_indicator_bulk(ticker_data_df=close_price,
+                                               ticker_list=ticker_list,
+                                               start_date=start_date,
+                                               end_date=end_date)
 
-        # Calculate performance metrics
-        performance_df = get_performance_metrics_bulk(ticker_data_df=close_price,
-                                                      ticker_list=ticker_list,
-                                                      market_data=market_data,
-                                                      start_date=start_date, end_date=end_date)
+            # Calculate performance metrics
+            performance_df = get_performance_metrics_bulk(ticker_data_df=close_price,
+                                                          ticker_list=ticker_list,
+                                                          market_data=market_data,
+                                                          start_date=start_date, end_date=end_date)
 
-        # Run all strategies
-        strategy_results = StrategyFactory(indicators_df=indicators_df,
-                                           performance_df=performance_df,
-                                           top_n=top_n_tickers).run_all_strategies()
+            # Run all strategies
+            strategy_results = StrategyFactory(indicators_df=indicators_df,
+                                               performance_df=performance_df,
+                                               top_n=top_n_tickers).run_all_strategies()
 
-        # Append results to the list
-        for strategy_name, tickers in strategy_results.items():
-            results.append({
-                'start_date': start_date,
-                'end_date': end_date,
-                'strategy_name': strategy_name,
-                'tickers': tickers
-            })
-        # Log the results for the current date range
-        logging.info(tabulate(strategy_results, headers='keys', tablefmt='psql'))
+            # Append results to the list
+            for strategy_name, tickers in strategy_results.items():
+                results.append({
+                    'start_date': start_date,
+                    'end_date': end_date,
+                    'strategy_name': strategy_name,
+                    'tickers': tickers
+                })
+            # Log the results for the current date range
+            logging.info(tabulate(strategy_results, headers='keys', tablefmt='psql'))
+        except Exception as e:
+            logging.error("Error=e , start_date={}, end_date={}".format(e, start_date, end_date))
     # Convert the results list to a DataFrame
     results_df = pd.DataFrame(results)
 
