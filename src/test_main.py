7516110158e32a94c0748307d972b93b97a05897
Rushikesh <rushikeshnakhate@gmail.com>
2025-08-17 08:18:42 +0800

    Update test_main.py

--- src/test_main.py
+++ src/test_main.py
@@ -1,4 +1,4 @@
 from src.main import run_pyport_ticker_selector
 
 if __name__ == "__main__":
-    run_pyport_ticker_selector(years=[2024], top_n_tickers=5)
+    get_top_tickers(years=[2024], top_n_tickers=5)
